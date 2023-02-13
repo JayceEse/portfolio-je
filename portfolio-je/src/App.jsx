79d51b592890c190823450008101776b744f454d
Jayce Ese <jayce.ese@gmail.com>
2023-02-13 13:13:52 +1000

    First commit, Setup

---
+++ portfolio-je/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Header, About, Projects, Contact, Footer} from './containers'
+import {Navbar} from './componets'
+
+const App = () => {
+  return (
+    <div className='App'>
+      
+    </div>
+  )
+}
+
+export default App
