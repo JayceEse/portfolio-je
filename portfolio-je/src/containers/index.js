c0a9093a49cc7bc9d991cf1aaca223be689c3e34
Jayce Ese <jayce.ese@gmail.com>
2023-02-16 15:46:35 +1000

    projects section done

--- portfolio-je/src/containers/index.js
+++ portfolio-je/src/containers/index.js
@@ -1,5 +1,11 @@
+//Sections to be exported to App.jsx
 export {default as Header} from './header/Header';
 export {default as About} from './about/About';
 export {default as Projects} from './projects/Projects';
 export {default as Contact} from './contact/Contact';
 export {default as Footer} from './footer/Footer';
+
+//assets to export to project section
+export {default as GPT3} from '../assets/gpt3-je-screenshot.png';
+export {default as Pokemon} from '../assets/Pokemon-stats-screenshot.png';
+export {default as Portfolio} from '../assets/portfolio-screenshot.png';
--- portfolio-je/src/containers/projects/Projects.jsx
+++ portfolio-je/src/containers/projects/Projects.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
-import './projects.css'
+import React from 'react';
+import './projects.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faReact, faJs, faCss3, faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faGlobe} from '@fortawesome/free-solid-svg-icons';
+import { GPT3, Pokemon, Portfolio } from '../index.js';
 
 const Projects = () => {
   return (
-    <div id='projects'>Projects</div>
+    <div className='projects section__padding' id='projects'>
+      <h1>Projects</h1>
+      <div className='projects__container'>
+        <div className='projects__content gradient__bg'>
+          <img src={GPT3} alt="gpt3 site screenshot" />
+          <div className='projects__content_info'>
+            <h3>GPT3 Modern Website</h3>
+            <div className='projects__content_links'>
+              <a href=""><FontAwesomeIcon icon={faGithub} /> Github Link Here</a>
+              <a href=""><FontAwesomeIcon icon={faGlobe} /> Live Link Here</a>
+            </div>
+            <p>Built a React website to demonstrate my understanding to code a Figma design into a responsive website using React.</p>
+            <ul>
+              <li><FontAwesomeIcon icon={faReact} id="react"/></li>
+              <li><FontAwesomeIcon icon={faJs} id="javascript"/></li>
+              <li><FontAwesomeIcon icon={faCss3} id="css"/></li>
+            </ul>
+          </div>
+        </div>
+        <div className='projects__content gradient__bg'>
+            <img src={Pokemon} alt="pokemon site screenshot" />
+            <div className='projects__content_info'>
+              <h3>Pokemon Stats API</h3>
+              <div className='projects__content_links'>
+                <a href=""><FontAwesomeIcon icon={faGithub} /> Github Link Here</a>
+                <a href=""><FontAwesomeIcon icon={faGlobe} /> Live Link Here</a>
+              </div>
+              <p>Created Pokemon Stats to learn how API requests work and add other features to enhance the users experience.</p>
+              <ul>
+                <li><FontAwesomeIcon icon={faReact} id="react"/></li>
+                <li><FontAwesomeIcon icon={faJs} id="javascript"/></li>
+                <li><FontAwesomeIcon icon={faCss3} id="css"/></li>
+                <li>Pokemon API</li>
+              </ul>
+            </div>
+        </div>
+        <div className='projects__content gradient__bg'>
+            <img src={Portfolio} alt="portfolio site screenshot" />
+            <div className='projects__content_info'>
+              <h3>Personal Portfolio Website</h3>
+              <div className='projects__content_links'>
+                <a href=""><FontAwesomeIcon icon={faGithub} /> Github Link Here</a>
+              </div>
+              <p>Created a responsive portfolio website showcasing my web development projects and skills.</p>
+              <ul>
+                <li><FontAwesomeIcon icon={faReact} id="react"/></li>
+                <li><FontAwesomeIcon icon={faJs} id="javascript"/></li>
+                <li><FontAwesomeIcon icon={faCss3} id="css"/></li>
+                <li>Vite JS</li>
+              </ul>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
