807b5b075fe08ee490e4e803819f47b039e3baaa
Jayce Ese <jayce.ese@gmail.com>
2023-02-16 11:09:30 +1000

    about section done

--- portfolio-je/src/containers/about/About.jsx
+++ portfolio-je/src/containers/about/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './about.css'
+import aboutPhoto from '../../assets/photoOfJayce.jpeg'
 
 const About = () => {
   return (
-    <div id='about'>About</div>
+    <div className='about section__padding' id='about'>
+    <div className='about__img_container'>
+      <img src={aboutPhoto} alt="Photo of Jayce" />
+    </div>
+    <div className='about__content_container'>
+      <h1>About Me</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis veniam dolorem, laudantium voluptatem ipsum ea doloremque accusamus ratione id delectus quasi mollitia debitis saepe ab possimus provident enim sit nemo.</p>
+    </div>
+    </div>
   )
 }
 
